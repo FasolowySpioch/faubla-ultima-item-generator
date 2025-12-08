4ba1c14087808ee2f481eb82f3c7f6aab7901d28
Aleksandra Ziemska <fasolowyspioch@gmail.com>
2025-12-08 20:11:59 +0100

    Poprawa  Basic_Weapons

--- fultima_gen/Enums.h
+++ fultima_gen/Enums.h
@@ -1,15 +1,15 @@
 #pragma once
 enum class Atributes {
-	WLP = 0, INS = 1, DEX = 2, MIG = 3
+	WLP = 0, INS = 1, DEX = 2, MIG = 3 //Jak w JSONIe jest cokolwiek powy¿ej 3 to zmusiæ generator do przypisania wartoœci która jest tam wpisana
 };
 enum class DMGType {
 	PHYSICAL = 0, AIR = 1, BOLT = 2, DARK = 3, EARTH = 4, FIRE = 5, ICE = 6, LIGHT = 7, POISON = 8
 };
 enum class StatusEffect { //Jeszcze nie wiem czy to potrzebne ale mo¿e siê przyda na potem
 	DAZED = 0, 
-	ENRAGED = 1, // Po³aczenie: Dazed + Slow. MOCNIEJSZE NI¯ PO PROSTU POJEDYNCZE!! PILNOWAÆ W KODZIE
-	POISONED = 2, // Po³¹czenie: Shaken + Weak. tak samo jak wy¿ej
-	SHAKEN = 3, 
-	SLOW = 4, 
-	WEAK = 5
+	ENRAGED = 4, // Po³aczenie: Dazed + Slow. MOCNIEJSZE NI¯ PO PROSTU POJEDYNCZE!! PILNOWAÆ W KODZIE
+	POISONED = 5, // Po³¹czenie: Shaken + Weak. tak samo jak wy¿ej
+	SHAKEN = 1, 
+	SLOW = 2, 
+	WEAK = 3
 };
