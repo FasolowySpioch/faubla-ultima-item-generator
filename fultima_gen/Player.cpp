fdf322ff41c6f121067744634b6f551b4e05d0ca
Aleksandra Ziemska <fasolowyspioch@gmail.com>
2025-12-08 20:16:27 +0100

    player fix

--- fultima_gen/Player.cpp
+++ fultima_gen/Player.cpp
@@ -3,7 +3,17 @@ int Player::getLevel() { return _level; }
 Atributes Player::getPrimaryDie1() { return _primary_die1; }
 Atributes Player::getPrimaryDie2() { return _primary_die2; }
 std::string Player::getPrefferedWeaponType() { return _preffered_weapon_type; }
-void Player::setLevel(int l) { _level = l; }
+bool Player::getCanMartialWeapon() { return _canMartialWeapon; }
+bool Player::getCanMartialArmor() { return _canMartialArmor; }
+bool Player::getCanRange() { return _canRange; }
+bool Player::getCanShield() { return _canShield; }
+std::string Player::getName() { return _name; }
+void Player::setLevel(int level) { _level = level; }
 void Player::setPrimaryDie1(Atributes pd1) { _primary_die1 = pd1; }
 void Player::setPrimaryDie2(Atributes pd2) { _primary_die2 = pd2; }
 void Player::setPrefferedWeaponType(std::string pwt) { _preffered_weapon_type = pwt; }
+void Player::setCanMartialWeapon(bool cmw) { _canMartialWeapon = cmw; }
+void Player::setCanMartialArmor(bool cma) { _canMartialArmor = cma; }
+void Player::setCanRange(bool cr) { _canRange = cr; }
+void Player::setCanShield(bool cs) { _canShield = cs; }
+void Player::setName(std::string name) { _name = name; }
--- fultima_gen/Player.h
+++ fultima_gen/Player.h
@@ -10,17 +10,40 @@ private:
 	std::string _preffered_weapon_type;
 	// std::string _preffered_armor_type; (?)
 	bool _canMartialWeapon, _canMartialArmor, _canRange, _canShield;
+	std::string _name;
 public:
-	Player() : _level(5), _primary_die1(Atributes::WLP), _primary_die2(Atributes::WLP), _preffered_weapon_type("") {}
-	Player(int level, Atributes primary_die1, Atributes primary_die2, std::string preffered_weapon_type) :
-		_level(level), _primary_die1(primary_die1), _primary_die2(primary_die2), _preffered_weapon_type(preffered_weapon_type) {}
+	Player() : _level(5), _primary_die1(Atributes::WLP), _primary_die2(Atributes::WLP),
+		_preffered_weapon_type(""), _canMartialWeapon(false), _canMartialArmor(false),
+		_canRange(false), _canShield(false), _name("") {
+	}
+	Player(int level, Atributes primary_die1, Atributes primary_die2,
+		std::string preffered_weapon_type, bool canMartialWeapon,
+		bool canMartialArmor, bool canRange, bool canShield,
+		std::string name) :
+		_level(level), _primary_die1(primary_die1), _primary_die2(primary_die2),
+		_preffered_weapon_type(preffered_weapon_type), _canMartialWeapon(canMartialWeapon),
+		_canMartialArmor(canMartialArmor), _canRange(canRange), _canShield(canShield),
+		_name(name) {
+	}
 	int getLevel();
 	Atributes getPrimaryDie1();
 	Atributes getPrimaryDie2();
 	std::string getPrefferedWeaponType();
-	void setLevel(int l);
+	bool getCanMartialWeapon();
+	bool getCanMartialArmor();
+	bool getCanRange();
+	bool getCanShield();
+	std::string getName();
+
+	void setLevel(int level);
 	void setPrimaryDie1(Atributes pd1);
 	void setPrimaryDie2(Atributes pd2);
 	void setPrefferedWeaponType(std::string pwt);
+	void setCanMartialWeapon(bool cmw);
+	void setCanMartialArmor(bool cma);
+	void setCanRange(bool cr);
+	void setCanShield(bool cs);
+	void setName(std::string name);
+	
 };
 
