99d58935a6b4445a513364f217f48632e380e3f3
FasolowySpioch <fasolowyspioch@gmail.com>
2026-01-05 18:53:17 +0100

    Preparation for writing tests

---
+++ src/tests/Test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ola on 5.01.2026.
+//
+
+#include "Test.h"
---
+++ src/tests/Test.h
@@ -0,0 +1,13 @@
+//
+// Created by ola on 5.01.2026.
+//
+
+#ifndef FULTIMA_GEN_TEST_H
+#define FULTIMA_GEN_TEST_H
+
+
+class Test {
+};
+
+
+#endif //FULTIMA_GEN_TEST_H
