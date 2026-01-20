40c3bc9cc22f8842a652ba9909fb361791e683c5
micjaw310589 <mj310589@student.polsl.pl>
2026-01-20 22:36:08 +0100

    wtf

--- gui/dialogue/itemgen/itemgendialogue.h
+++ gui/dialogue/itemgen/itemgendialogue.h
@@ -14,11 +14,7 @@ class ItemGenDialogue : public QDialog
     Q_OBJECT
 
 public:
-<<<<<<< HEAD
     explicit ItemGenDialogue(const std::vector<std::unique_ptr<Player>> &_players, QWidget *parent = nullptr);
-=======
-    explicit ItemGenDialogue(std::vector<Player>& players, QWidget *parent = nullptr);
->>>>>>> 231168f40ea90e510cf1cec36e588c4912b31546
     ~ItemGenDialogue();
 
 private slots:
@@ -26,12 +22,8 @@ private slots:
 
 private:
     Ui::ItemGenDialogue *ui;
-<<<<<<< HEAD
     const std::vector<std::unique_ptr<Player>> &players;
-=======
     void setLayoutVisible(bool visible, QLayout* l);
-    std::vector<Player>& players;
->>>>>>> 231168f40ea90e510cf1cec36e588c4912b31546
 };
 
 #endif // ITEMGENDIALOGUE_H
