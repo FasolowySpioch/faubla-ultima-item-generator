4829ac83c1c3df8e1a55e5184724a5c064b0c01e
Aleksandra Ziemska <fasolowyspioch@gmail.com>
2026-01-21 02:11:07 +0100

    hotfix

--- main.cpp
+++ main.cpp
@@ -8,6 +8,12 @@
 
 void testWeaponGenerator();
 
+//TODO: Delete button doesn't work.
+//TODO: ItemGenerator dialog window.
+//TODO: LoadCampain button dosn't work -> reload tables to have accurate data
+//TODO: Correct model after deleting player
+//TODO: (Optional) delete items with doubleclick - maybe.
+
 int main(int argc, char *argv[])
 {
      QApplication a(argc, argv);
