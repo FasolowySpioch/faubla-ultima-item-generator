e21112b0d6941aed0258e2f63d818c426e987d3c
micjaw310589 <mj310589@student.polsl.pl>
2026-01-21 13:55:30 +0100

    updated TODO list in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,9 @@
 #include "gui/mainwindow.h"
 #include "src/tests/Test.h"
-
-
 #include <QApplication>
 
 //TODO: Delete button doesn't work.
 //TODO: ItemGenerator dialog window.
-//TODO: LoadCampain button dosn't work -> reload tables to have accurate data
-//TODO: Correct model after deleting player
 //TODO: (Optional) delete items with doubleclick - maybe.
 //TODO: (Optional) change DeleteCampain btn to just clear the app / Add missing clear feature
 
